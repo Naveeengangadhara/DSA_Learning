641efa5739aa44668759b1f9113502df20fd0422
Naveen <kandranaveen650@gmail.com>
2025-02-01 08:45:53 -0500

    solution commited

---
+++ Sorting/Special_Array_I.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Special_Array_I:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(n - 1):
+            if nums[i] % 2 == nums[i + 1] % 2:
+                return False
+        return True
